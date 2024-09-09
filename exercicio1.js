ede66975073e230fb7c1335c400c649c16c7b847
JoaoRViana <joaorviana98@gmail.com>
2024-09-09 17:30:17 -0300

    exercicio 1

---
+++ exercicio1.js
@@ -0,0 +1,12 @@
+function exercicio(){
+    let indice =13;
+    let soma = 0;
+    let k = 0
+    while(k<indice){
+        k= k+1;
+        soma = soma+k
+    }
+    console.log(soma)
+}
+exercicio()
+//reposta 91
